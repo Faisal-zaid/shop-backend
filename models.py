963dfd115efc8a0e19a633d9be1d929ca8bd77eb
Faisal Zaid <zaidalideveloper@gmail.com>
2025-12-01 23:19:10 +0300

    added user table

---
+++ models.py
@@ -0,0 +1,16 @@
+#from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from sqlalchemy import Column, Integer,Text,DateTime
+from sqlalchemy.orm import declarative_base
+
+#setup base class where our models will inherit from
+Base=declarative_base()
+
+#start creating the schema
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer(), primary_key=True)
+    name = Column(Text(), nullable=False)
+    email = Column(Text(), unique=True,  nullable=False)
+    role = Column(Text(), default="staff")
+    created_at = Column(DateTime, default=datetime. now)
