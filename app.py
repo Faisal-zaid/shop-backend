79050ca5d299de5faf103b701615dedf181c7d2d
Faisal Zaid <zaidalideveloper@gmail.com>
2025-12-02 17:53:45 +0300

    added fast api methods

---
+++ app.py
@@ -0,0 +1,38 @@
+#1. import fast api
+from fastapi import FastAPI
+
+#create and instance
+app = FastAPI()
+
+
+#create routes and instances
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+#http://localhost:8000/User -> POST-> create a single genre
+@app.post("/User")
+def create_user():
+    #use sql alchemy to create records
+    return {"message":"User created succesfully"}
+
+#http://localhost:8000/User -> GET-> retrieve all genre
+@app.get("/User")
+def get_user():
+    #use sql alchemy to retrieve all users 
+    return[]
+
+#http://localhost:8000/User -> GET-> get a single genre
+@app.get("/User/{User_id}")
+def get_user(User_id):
+    return{}
+
+#http://localhost:8000/User -> PATCH-> update a single genre
+@app.patch("/User/{User_id}")
+def update_user(User_id):
+    return{}
+
+#http://localhost:8000/User -> DELETE-> create a single genre
+@app.delete("/User/{User_id}")
+def delete_user(User_id):
+    return{}
