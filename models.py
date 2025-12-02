19a93c0afcc76d837040c3b67e0dbc4e46e5c890
Faisal Zaid <zaidalideveloper@gmail.com>
2025-12-02 17:07:06 +0300

    added products and order table

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 #from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-from sqlalchemy import Column, Integer,Text,DateTime
+from sqlalchemy import Column, Integer,Text,DateTime, ForeignKey
 from sqlalchemy.orm import declarative_base
 
 #setup base class where our models will inherit from
@@ -20,4 +20,23 @@ class Category(Base):
     id = Column(Integer, primary_key=True, )
     name = Column(Text, nullable=False, unique=True)
     description = Column(Text)
-    created_at = Column(DateTime, default=datetime.now)    
+    created_at = Column(DateTime, default=datetime.now) 
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(Text, nullable=False)
+    price = Column(Integer, nullable=False)
+    stock = Column(Integer, default=0)
+    category_id = Column(Integer, ForeignKey("categories.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    created_at = Column(DateTime, default=datetime.now)  
+
+class Order(Base):
+    __tablename__ = "orders"
+    id = Column(Integer, primary_key=True, index=True)
+    product_id = Column(Integer, ForeignKey("products.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)  # who recorded the order
+    quantity = Column(Integer, nullable=False, default=1)
+    total_price = Column(Integer)
+    created_at = Column(DateTime, default=datetime.utcnow)         
