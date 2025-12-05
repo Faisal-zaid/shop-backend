df026abf803def700c55bfd4f035f09c375d5da1
Faisal Zaid <zaidalideveloper@gmail.com>
2025-12-05 15:09:01 +0300

    categories updated to add categories in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #1. import fast api
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 #this is an inbuilt package in python which allows us to define the shape of POST and PATHCH methods and aslso do validations
 from pydantic import BaseModel
 from models import get_db, User, Category, Product, Order
@@ -47,15 +47,30 @@ class CategorySchema(BaseModel):
 
 #http://localhost:8000/User -> POST-> create a single genre
 @app.post("/Category")
-def create_category(category:CategorySchema):
-    #use sql alchemy to create records
+def create_category(category:CategorySchema, session = Depends(get_db)):
+    #check if the genre exists
+    existing=session.query(Category).filter(Category.name == category.name).first()
+
+
+    if existing is not None:
+        return {"message":"Category already exists"}
+    #print(category) #this will be a dict
+    #persist to db
+    #1.create an instance of category class(model)with the details
+    new_category=Category(name=category.name)
+    #add the instance to the transaction
+    session.add(new_category)
+    #3.commit the transaction
+    session.commit()
+    #return a message that the category has been created 
     return {"message":"Category created succesfully"}
 
 #http://localhost:8000/User -> GET-> retrieve all genre
 @app.get("/Category")
-def get_category():
+def get_category(session = Depends(get_db)):
     #use sql alchemy to retrieve all users 
-    return[]
+    category = session.query(Category).all()
+    return category
 
 #http://localhost:8000/User -> GET-> get a single genre
 @app.get("/Category/{Category_id}")
